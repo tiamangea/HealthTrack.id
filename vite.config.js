56e66ab902929cd9234613b4c7243bdcc79a765e
tiamangea <119683815+tiamangea@users.noreply.github.com>
2026-05-10 22:29:00 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/nama-repository-kamu/', // Ganti dengan nama repo yang tepat
+  base: '/https://github.com/tiamangea/HealthTrack.id/', // Ganti dengan nama repo yang tepat
   plugins: [react()],
 })
