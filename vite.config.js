27a34459da77a1a172b39b39de67b166da2ff3fb
tiamangea <119683815+tiamangea@users.noreply.github.com>
2026-05-10 22:25:27 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/nama-repository-kamu/', // Ganti dengan nama repo yang tepat
   plugins: [react()],
 })
